f960181ede7d80cd3429f6b0327eee55d318487c
Ankita Arya <ankitaarya1207@gmail.com>
2025-04-15 19:02:29 +0530

    navbar

--- url-shortener/app/page.js
+++ url-shortener/app/page.js
@@ -3,7 +3,19 @@ import Image from "next/image";
 export default function Home() {
   return (
    <main>
-    I am main
+    <section className="grid grid-cols-2">
+      <div className="bg-green-50">
+        <p>
+          The best URL Shortener in the market
+        </p>
+        <p>
+          We are the most straightforward URL Shortener in the world
+        </p>
+      </div>
+      <div className="bg-red-500">
+      <Image alt="an image of vector" src={"/vector.jpg"} width={300} height={300} />
+      </div>
+    </section>
    </main>
   );
 }
